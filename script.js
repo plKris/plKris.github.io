2bcfb3951bfa24c0326ff06167bf1b87617de965
Kris <krisplesel@gmail.com>
2025-09-13 15:01:13 +0200

    Modification des contacts

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-// Animation du logo
-gsap.fromTo(
-  ".logo-name",
-  { y: 50, opacity: 0 },
-  { y: 0, opacity: 1, duration: 2, delay: 0.5 }
-);
-
-// Animation de la disparition de la loading-page
-gsap.fromTo(
-  ".loading-page",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    display: "none",
-    duration: 1.5,
-    delay: 3.5,
-    onComplete: () => {
-      // Affiche le wrapper après disparition de la loading page
-      const wrapper = document.querySelector(".wrapper");
-      wrapper.style.display = "flex";
-      wrapper.style.flexDirection = "column"; // centre vertical et horizontal
-    }
-  }
-);
+// Assurer que le wrapper est visible dès le départ
+const wrapper = document.querySelector(".wrapper");
+wrapper.style.display = "flex";
+wrapper.style.flexDirection = "column";
 
 // Redirection au clic sur le wrapper
-const wrapper = document.querySelector(".wrapper");
 wrapper.addEventListener("click", () => {
   window.location.href = "pages/apropos.html";
 });
-
